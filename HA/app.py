b5e976e7588aec7586799680cda90f6a66751888
liangxin1300 <XLiang@suse.com>
2019-04-12 10:17:30 +0800

    test3

--- HA/app.py
+++ HA/app.py
@@ -1,5 +1,21 @@
 import subprocess
 
+
+def to_ascii(s):
+    """Convert the bytes string to a ASCII string
+    Usefull to remove accent (diacritics)"""
+    if s is None:
+        return s
+    if isinstance(s, str):
+        return s
+    try:
+        return str(s, 'utf-8')
+    except UnicodeDecodeError:
+        import traceback
+        traceback.print_exc()
+        return s
+
+
 def get_stdout(cmd, input_s=None, stderr_on=True, shell=True):
     if stderr_on:
         stderr = None
@@ -11,8 +27,11 @@ def get_stdout(cmd, input_s=None, stderr_on=True, shell=True):
                             stdout=subprocess.PIPE,
                             stderr=stderr)
     stdout_data, stderr_data = proc.communicate(input_s)
-    return proc.returncode, stdout_data
+    return proc.returncode, to_ascii(stdout_data)
 
+
+print(get_stdout("which ip")[1])
+print(get_stdout("ip addr show")[1])
 print(get_stdout("rpm -qa crmsh")[1])
 print(get_stdout("rpm -qa pacemaker")[1])
 print(get_stdout("rpm -qa corosync")[1])
