530f0501d769e655a3128123948755e4c486940a
liangxin1300 <XLiang@suse.com>
2019-12-12 14:49:22 +0800

    update

--- HA/ha/run.sh
+++ HA/ha/run.sh
@@ -1,17 +1,30 @@
 #!/bin/bash
-Tumbleweed_image='liangxin1300/ha'
+image='liangxin1300/ha'
 
 setup() {
   num_container=${1:-1}
   if ! [[ $num_container =~ ^[1-9][0-9]*$ ]];then
-    echo "bad"
+    echo "Usage: setup number"
     return
   fi
-  echo $num_container
-  #docker pull ${Tumbleweed_image}
-  #docker network create --subnet 10.10.10.0/24 second_net
 
-  #docker run -d --name=hanode1 --hostname hanode1 --privileged -v /sys/fs/cgroup:/sys/fs/cgroup:ro -v ${Tumbleweed_image}
+  #for i in $(seq $num_container)
+  #do
+  #  echo "10.10.10.$((i+1)) hanode$i"
+  #done
+  docker pull ${image}
+  docker network create --subnet 10.10.10.0/24 second_net
+  
+  for i in $(seq $num_container)
+  do
+    _hostname="hanode$i"
+    _ip="10.10.10.$((i+1))"
+    echo "node: $_hostname IP: $_ip"
+    
+    docker run -d --name=$_hostname --hostname $_hostname --privileged -v /sys/fs/cgroup:/sys/fs/cgroup:ro ${image}
+    docker network connect --ip=$_ip second_net $_hostname
+  done
+
   #docker network connect --ip=10.10.10.2 second_net hanode1
   #docker exec -t hanode1 /bin/sh -c "echo \"10.10.10.3 hanode2\" >> /etc/hosts"
 }
