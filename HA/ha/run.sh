be97c2b0a04a12df833974c9ffe4e6558a947eed
liangxin1300 <XLiang@suse.com>
2019-12-12 15:14:47 +0800

    update

--- HA/ha/run.sh
+++ HA/ha/run.sh
@@ -2,11 +2,6 @@
 image='liangxin1300/ha'
 
 setup() {
-  num_container=${1:-1}
-  if ! [[ $num_container =~ ^[1-9][0-9]*$ ]];then
-    echo "Usage: setup number"
-    return
-  fi
 
   docker pull ${image}
   docker network create --subnet 10.10.10.0/24 second_net
@@ -15,7 +10,7 @@ setup() {
   do
     _hostname="hanode$i"
     _ip="10.10.10.$((i+1))"
-    echo "node: $_hostname IP: $_ip"
+    echo "Node: $_hostname IP: $_ip"
     
     docker run -d --name=$_hostname --hostname $_hostname --privileged -v /sys/fs/cgroup:/sys/fs/cgroup:ro ${image}
     docker network connect --ip=$_ip second_net $_hostname
@@ -31,4 +26,23 @@ setup() {
   done
 }
 
-setup "$1"
+clean() {
+  for i in $(seq $num_container)
+  do
+    _hostname="hanode$i"
+    docker container stop $_hostname 2> /dev/null
+    docker container rm $_hostname 2> /dev/null
+  done
+}
+
+num_container=${2:-1}
+if ! [[ $num_container =~ ^[1-9][0-9]*$ ]];then
+  echo "Usage: setup/clean number"
+  return
+fi
+
+case "$1" in
+  "setup") setup;;
+  "clean") clean;;
+  *) echo "Usage: setup/clean number";;
+esac
